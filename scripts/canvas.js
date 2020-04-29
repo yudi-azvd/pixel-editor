bd51bac845b17e3cc313c7b6bd9314d49fa6d0bb
Yudi Yamane <yudiyamane@gmail.com>
2020-04-28 22:55:06 -0300

    fixed pixel warping

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,13 +1,13 @@
 class Canvas {
   constructor(document, id) {
     this.canvas = document.getElementById(id)
+    this.GRID_PIXELS = 32
+    this.canvas.width = this.GRID_PIXELS
+    this.canvas.height = this.GRID_PIXELS
     this.CANVAS_CSS_WIDTH = 400
     this.CANVAS_CSS_HEIGHT = 400
     this.canvas.style.width = this.CANVAS_CSS_WIDTH
     this.canvas.style.height = this.CANVAS_CSS_HEIGHT
-    this.GRID_PIXELS = 32
-    this.width = this.GRID_PIXELS
-    this.height = this.GRID_PIXELS
     this.canvasBounding = this.canvas.getBoundingClientRect()
     /** @type {CanvasRenderingContext2D} */
     this.context = this.canvas.getContext('2d')
@@ -24,7 +24,7 @@ class Canvas {
       let mappedX = Math.trunc( originalX * this.GRID_PIXELS / this.CANVAS_CSS_WIDTH )
       let mappedY = Math.trunc( originalY * this.GRID_PIXELS / this.CANVAS_CSS_HEIGHT )
       
-      console.log(originalX, event.clientX, this.canvasBounding.left, mappedX)
+      console.log(originalX, mappedX)
       this.isDrawing = true
       pixelDrawer.draw(mappedX, mappedY)
     })
